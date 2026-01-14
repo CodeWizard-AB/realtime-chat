b66e5c703d55a9cd2972a1c9c66e3b8bca95a8a6
Anunay <adobex092@gmail.com>
2026-01-14 21:11:58 +0600

    feat: Add Elysia framework integration and update dependencies

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,14 @@
+import { Elysia, t } from "elysia";
+
+export const app = new Elysia({ prefix: "/api" })
+	.get("/", {
+		user: { name: "John" },
+	})
+	.post("/", ({ body }) => body, {
+		body: t.Object({
+			name: t.String(),
+		}),
+	});
+
+export const GET = app.fetch;
+export const POST = app.fetch;
